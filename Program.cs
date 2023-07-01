b668de6a467aa0a781829ddbd9de88be7c29064d
Ioane Mamulashvili <ioane.mamulashvili@gmail.com>
2023-07-01 13:06:12 +0400

    Initial Commit

--- Program.cs
+++ Program.cs
@@ -6,5 +6,14 @@ namespace TicRickPrick {
 			Console.WriteLine("Hello World, Are you ready?");
 			Console.ReadLine();
 		}
+		static void Divide(int a, int b) {
+			Console.WriteLine(a / b);
+		}
+		static void Power(int a) {
+			Console.WriteLine(a * a);
+		}
+		static void Multiply(int a, int b) {
+			Console.WriteLine(a * b);
+		}
 	}
 }
