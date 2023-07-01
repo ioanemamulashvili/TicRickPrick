91a5e88985774d3d6c69fa50773c4f35cc2e58e9
Ioane Mamulashvili <ioane.mamulashvili@gmail.com>
2023-07-01 12:47:51 +0400

    Console.Writeline Modified

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ namespace TicRickPrick {
 	internal class Program {
 		static void Main(string[] args) {
 			Console.WriteLine("Hello World, How are ya?!");
+			Console.WriteLine("Feeling Tired, But I gotta go forward.");
 			Console.ReadLine();
 		}
 	}
