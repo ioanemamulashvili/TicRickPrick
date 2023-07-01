192b2867090260304a95f9e1e8c87fe814326235
Ioane Mamulashvili <66328288+ioanemamulashvili@users.noreply.github.com>
2023-07-01 12:50:51 +0400

    Modified Message Again in Program.cs

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@
 namespace TicRickPrick {
 	internal class Program {
 		static void Main(string[] args) {
-			Console.WriteLine("Hello World, How are ya?!");
+			Console.WriteLine("Hello World, How are ya?! Need a break?");
 			Console.WriteLine("Feeling Tired, But I gotta go forward.");
 			Console.ReadLine();
 		}
